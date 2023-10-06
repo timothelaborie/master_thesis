29e8ce0bf1f3f0f05c2127b2ef5c5bb6e8bcde95
timothelaborie <tlaborie@student.ethz.ch>
2023-10-06 14:59:05 +0200

    full data cleanup code

--- bitcoinforum/crawler_code/preprocessing.sh
+++
@@ -1,7 +0,0 @@
-python preprocessing.py /local/home/puwong/bitcoin/data/mining_support
-python preprocessing.py /local/home/puwong/bitcoin/data/mining_speculation
-python preprocessing.py /local/home/puwong/bitcoin/data/miners
-python preprocessing.py /local/home/puwong/bitcoin/data/hardware
-python preprocessing.py /local/home/puwong/bitcoin/data/groupbuys
-python preprocessing.py /local/home/puwong/bitcoin/data/pools
-python preprocessing.py /local/home/puwong/bitcoin/data/mining
